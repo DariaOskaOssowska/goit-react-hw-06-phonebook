0c715a93bcc8af72485a5c0017036018beb85495
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-11-28 23:16:26 +0100

    Refaktor store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './reducer';
 
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: rootReducer,
+});
